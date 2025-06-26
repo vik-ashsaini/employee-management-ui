de38305eac1aee386fff1c0cafee8ff1ec1914fc
vik-ashsaini <vikashsaini879@gmail.com>
2025-06-26 09:46:52 +0530

    update for Login UI

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './login.component.html'
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
   form!: FormGroup;
@@ -17,15 +29,17 @@ export class LoginComponent implements OnInit{
 
   ngOnInit(){
     this.form = this.fb.group({
-      email: [''],
-      password: ['']
+      email: ['',[Validators.required,Validators.email]],
+      password: ['',[Validators.required,Validators.minLength(6)]]
     });
   }
 
   submit() {
-    this.auth.login(this.form.value).subscribe({
-      next: () => this.router.navigate(['/']),
-      error: () => alert('Login failed')
-    });
+    if(this.form.valid){
+      this.auth.login(this.form.value).subscribe({
+        next: () => this.router.navigate(['/employee']),
+        error: () => alert('Login failed')
+      });
+    }    
   }
 }
